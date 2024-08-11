dc6e9e1dcb2c36a42ffe660dd8e15637718a9e4f
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-08-11 18:33:12 -0400

    Initial Commit

---
+++ Firebase Bootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Firebase Bootcamp
+//
+//  Created by Parth Antala on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Firebase Bootcamp/Firebase_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_BootcampApp.swift
+//  Firebase Bootcamp
+//
+//  Created by Parth Antala on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
